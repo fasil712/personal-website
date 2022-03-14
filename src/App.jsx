d97e9095a36d440ad44fbc547da0950b8200956a
Fasil Getie <51247312+fasil712@users.noreply.github.com>
2022-03-14 13:41:35 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Experience from './components/experience/Experience'
+import Testimonials from './components/testimonials/Testimonials'
+import Services from './components/services/Services'
+import Contact from './components/contact/Contact'
+import Portfolio from './components/portfolio/Portfolio'
+import About from './components/about/About'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>     
+      <Services/>
+      <Portfolio/>   
+      <Testimonials/>       
+      <Contact/>  
+      <Footer/>
+    </>
   )
 }
-
 export default App
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Fasil Getie</h1>
+        <h5 className="text-light">
+          Fullstack Developer
+        </h5>
+        <CTA/>
+        <HeaderSocials/>
+      </div>
+      <div className="me">
+        <img src={ME} alt="Me" />
+      </div>
+      <a href="#contact" className='scroll_down'>Scroll Down</a>
+    </header>
   )
 }
 
